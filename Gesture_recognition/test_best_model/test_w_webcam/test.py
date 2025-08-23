8f33f205b7fdcd42275b0b0b60c5e26b97648767
chikien07012006 <kienduong160@gmail.com>
2025-08-23 14:00:06 +0700

    ok

---
+++ Gesture_recognition/test_best_model/test_w_webcam/test.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO(r"Gesture_recognition\YOLO\runs\train\exp1\weights\last.pt")  
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    results = model.predict(frame, conf=0.5)  
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLO11 Webcam Test", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
